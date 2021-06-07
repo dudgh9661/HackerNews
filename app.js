dffc96c0c642858fe922f8d1ce6169103de88954
Kimyeongho <dudgh9661@naver.com>
2021-06-07 21:14:12 +0900

    modified : DOM API 최소화

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
-
-console.log(newsFeed);
-
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function() {
+    const id = location.hash.substr(1);
+    
+
+    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+
+    content.appendChild(title);
+});
+
 for(let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
     const li = document.createElement('li');
-    li.innerHTML = newsFeed[i].title;
-    ul.appendChild(li);
+    const a = document.createElement('a');
+    
+    div.innerHTML = `
+    <li>
+        <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})></a>
+    </li>
+    `;
+    ul.appendChild(div.firstElementChild);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
